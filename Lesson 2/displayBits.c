6e3b8a3b419422b3705788cb0f6fcbbc1d6207f7
ido weinstock <ido.weinstock@gmail.com>
2026-03-15 19:28:39 +0200

    Adding display bits code

---
+++ Lesson 2/displayBits.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+void displayBits( unsigned value );
+
+
+void main()
+{
+	unsigned value = 5;
+	displayBits( value );
+
+	puts("");
+}
+
+
+void displayBits( unsigned value )
+{ 
+   unsigned c; /* counter */
+   
+   /* declare displayMask and left shift 31 bits */
+   unsigned displayMask = 1 << 31;
+
+   printf( "%7u = ", value );
+
+   /* loop through bits */
+   for ( c = 1; c <= 32; c++ ) { 
+      putchar( value & displayMask ? '1' : '0' );
+      value <<= 1; /* shift value left by 1 */
+
+      if ( c % 8 == 0 ) { /* output a space after 8 bits */
+         putchar( ' ' );
+      } /* end if */
+
+   } /* end for */
+
+   putchar( '\n' );
+} /* end function displayBits */
