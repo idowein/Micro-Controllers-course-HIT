5700d4be7d2613c21caf8cf8eec85c17c6cd0f95
ido weinstock <ido.weinstock@gmail.com>
2026-03-15 20:17:18 +0200

    coding first task second way

--- Exercises/Class work/Exercise1.c
+++ Exercises/Class work/Exercise1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-unsigned int inverBits(unsigned int value){
+// Task 1
+unsigned int inverBitsUsingXor(unsigned int value){
     unsigned int mask = 0xFFFFFFFF; // mask definition
     // Note: unsigned - MSB is not a sign bit. always positive value
     // int - 32 bits, 4 bytes, 2 words.
@@ -8,3 +9,10 @@ unsigned int inverBits(unsigned int value){
     // 8 * F = 32 * 1 = 1111 ... 11 (32 times)
     return value ^ mask; // ^ = XOR operation
 }
+
+// Task 1 - second way (using not gate)
+// Note - not to check!! only for myself
+unsigned int inverBitsUsingNot(unsigned int value){
+    return ~value; // ~ filps each bit
+}
+
