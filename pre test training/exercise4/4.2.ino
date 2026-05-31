d05cf396138fdbe474a4c15deff13a55cbb96c5f
ido weinstock <ido.weinstock@gmail.com>
2026-05-31 10:18:32 +0300

    updating 4.2 - working with ISR

--- pre test training/exercise4/4.2.ino
+++ pre test training/exercise4/4.2.ino
@@ -1,39 +1,39 @@
 // Defining the Pin numbers as macros for better code maintenance
-#define BUTTON_PIN      2    // Button is on Port D, Pin 2 (INT0 - wired in the board)
-// pin3 wired to INT1
+#define BUTTON_PIN      2    // Button is on Port D, Pin 2 (INT0)
 #define GREEN_LED_PIN   5    // Green LED is on Port B, Pin 5 (Digital Pin 13)
 
 void setup() {
     /* 1. Configure I/O Directions using (1 << pin) */
-    // Clear bit 3 in DDRD to set it as INPUT
-    DDRD &= ~(1 << BUTTON_PIN); 
-    
-    // Set bit 4 in DDRB to set it as OUTPUT
-    DDRB |= (1 << GREEN_LED_PIN);  
+    DDRD &= ~(1 << BUTTON_PIN); // Set Pin 2 as INPUT
+    DDRB |= (1 << GREEN_LED_PIN); // Set Pin 13 as OUTPUT
   
-  	EICRA &= 0b11111100; // register zero configuration
+    // Zeroing out the INT0 control bits (ISC01 and ISC00) safely
+    EICRA &= ~((1 << ISC01) | (1 << ISC00)); 
   
-    /* 2. Configure External Interrupt Control Register A (EICRA) */
-    // Set INT0 to trigger on a LOGIC CHANGE (ISC01 = 0, ISC00 = 0)
-    EICRA |= (1 << 0); 
+    /* 2. Configure EICRA for Any Logical Change */
+    // Setting ISC01 = 0, ISC00 = 1 triggers the ISR on both PRESS and RELEASE
+    EICRA |= (1 << ISC00);  
 
     /* 3. Configure External Interrupt Mask Register (EIMSK) */
-    // Enable External Interrupt Request 1 (INT0)
-  	EIMSK |= (1 << INT0);
+    EIMSK |= (1 << INT0); // Enable INT0
 
     /* 4. Enable Global Interrupts */
     sei(); 
 }
 
 void loop() {
-    // Main loop remains empty. The microcontroller waits in hardware for the interrupt.
+    // Empty
 }
 
 /* 5. Interrupt Service Routine (ISR) for INT0 */
 ISR(INT0_vect) {
-    // Toggle the Green LED using the (1 << pin) configuration inside the XOR operation
-    if(PIND & (1<<BUTTON_PIN))	
-  		PORTB |= (1 << GREEN_LED_PIN); 
-    else
-       	PORTB &= ~(1 << GREEN_LED_PIN);
+    // Read the current physical state of Port D Pin 2
+    if (PIND & (1 << BUTTON_PIN)) {
+        // Button is currently PRESSED (Signal is HIGH due to external pull-down)
+        PORTB |= (1 << GREEN_LED_PIN);  // Turn ON the LED
+    } 
+    else {
+        // Button is currently RELEASED (Signal dropped to LOW)
+        PORTB &= ~(1 << GREEN_LED_PIN); // Turn OFF the LED
+    }
 }
